015257a5a0007ecdcb9364b3f043b00bfad4563e
salvatore naro <salvatorenaro009@gmail.com>
2025-06-23 16:44:48 +0200

    Update dog_vs_cats.py

--- dog_vs_cats.py
+++ dog_vs_cats.py
@@ -136,7 +136,7 @@ model.compile(loss='binary_crossentropy',optimizer=optimizers.RMSprop(learning_r
 #----------------------------------------------------------------------------------------------------------
 
 #Ri-scala tutte le imaggini di 1/255
-train_datagen = ImageDataGenerator(rescale = 1./255)
+train_datagen = ImageDataGenerator(rescale = 1./255,rotation_range=40,width_shift_range=0.2,height_shift_range=0.2,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 train_generator = train_datagen.flow_from_directory(
